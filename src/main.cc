af9cc407b9b5f26623f9bb7da04751bb3cca0f93
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-12-11 00:03:49 -0200

    main.cc: simple seastar program
    Adds a dummy seastar program to test if the build system is linking and
    compiling properly using the docker image
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,19 @@
+#include "core/app-template.hh"
 
+using namespace seastar;
+using namespace std;
 
-int main(int argc, char**)
+int main(int argc, char** argv)
 {
-	return 0;
+	namespace bpo = boost::program_options;
+	app_template app;
+	app.add_options()
+		("warcfile", bpo::value<string>()->default_value(""),
+		 "WARC file to process");
+	return app.run(argc, argv, [&] {
+		auto&& config = app.configuration();
+		string file = config["warcfile"].as<string>();
+		cout << "Parsing: " << file << endl;
+            	return seastar::make_ready_future<>();
+	});
 }
